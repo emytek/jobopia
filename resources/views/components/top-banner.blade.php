19e7c75f39e71ecc70b3f7125a9ede2f1841a29e
Nnaemeka Okalla <emyokalla@gmail.com>
2024-11-20 09:05:19 +0100

    jobopia-full-development

---
+++ resources/views/components/top-banner.blade.php
@@ -0,0 +1,13 @@
+@props(['heading' => 'Unlock Your Career Potential', 'subheading' => 'Discover the perfect job opportunity for you'])
+
+<section class="bg-green-900 text-white py-6 text-center">
+    <div class="container mx-auto">
+        <h2 class="text-xl md:text-3xl font-semibold">
+            {{$heading}}
+        </h2>
+        <p class="text-base sm:text-lg md:text-xl lg:text-2xl mt-2">
+           {{$subheading}}
+        </p>
+    </div>
+</section>
+
